e5daaf3c436c8acb886f7d599190a414cdce6c6a
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-12-15 18:28:15 +0530

    commits on 15 dec 2025

--- rating1400/1883G1.cpp
+++ rating1400/1883G1.cpp
@@ -13,9 +13,44 @@ using vll = vector<pair<ll, ll>>;
 
 int T;
 
+bool check(int ops, vi a, vi b)
+{
+    for(int i = 0; i < ops; i++){
+        a.pop_back();
+        b.pop_back();
+    }
+    reverse(b.begin(), b.end());
+
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] >= b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 void solve()
 {
-    
+    int n, m;
+    cin >> n >> m;
+    vi a(n);
+    vi b(n);
+    a[0] = 1;
+    for(int i = 1; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.rbegin(), b.rend());
+    int low = 0, high = n;
+
+    while( low < high){
+        int mid = (low + high) / 2;
+        if(check(mid, a, b)){
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    cout << low << nl;
 
     return;
 }
