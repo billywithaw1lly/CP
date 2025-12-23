692833451fb4b7019f7813ab0af2231f08d90368
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-12-24 00:37:22 +0530

    update

--- contest/E.cpp
+++ contest/E.cpp
@@ -15,41 +15,52 @@ int T;
 
 void solve()
 {
-    ll n, x, y; 
+    ll n, x, y;
     cin >> n >> x >> y;
     string s;
     cin >> s;
 
     vi p(n);
     ll req = 0;
-    for(auto &it : p){
+    for (auto &it : p)
+    {
         cin >> it;
         req += it;
     }
-    
-    if(req > (x + y)){
+    if (req > (x + y))
+    {
         cout << "NO" << nl;
         return;
     }
-    //0 -> a & 1 -> b
 
     ll extra = x + y - req;
     ll winA = 0;
     ll winB = 0;
-    ll a = 0;
-    ll b = 0;
+    ll a_districts = 0;
+    ll b_districts = 0; 
+    ll evenA = 0;       
+    ll evenB = 0; 
 
-    for(int i = 0; i < n; i++){
-        if(s[i] == '0'){
-            winA += (p[i] / 2) + 1;
-            a++;
+    for (int i = 0; i < n; i++)
+    {
+        ll cost = (p[i] / 2) + 1;
+        if (s[i] == '0')
+        {
+            winA += cost;
+            a_districts++;
+            if (p[i] % 2 == 0)
+                evenA++;
         }
-        else {
-            winB += (p[i] / 2) + 1;
-            b++;
+        else
+        {
+            winB += cost;
+            b_districts++;
+            if (p[i] % 2 == 0)
+                evenB++;
         }
     }
-    if(winA > x || winB > y){
+
+    if (winA > x || winB > y){
         cout << "NO" << nl;
         return;
     }
@@ -57,6 +68,22 @@ void solve()
     ll leftA = x - winA;
     ll leftB = y - winB;
 
+    ll max_add_A = 0;
+    if (a_districts > 0){
+        max_add_A = evenA + (leftA * 2);
+    }
+
+    ll max_add_B = 0;
+    if (b_districts > 0){
+        max_add_B = evenB + (leftB * 2);
+    }
+
+    if (extra <= max_add_A + max_add_B){
+        cout << "YES" << nl;
+    }
+    else{
+        cout << "NO" << nl;
+    }
 }
 
 int main()
