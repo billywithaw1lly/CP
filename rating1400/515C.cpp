d050e70c1d6b928c1b2decd73110c207f1c70c12
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-12-25 00:17:42 +0530

    update

--- rating1400/515C.cpp
+++ rating1400/515C.cpp
@@ -19,7 +19,21 @@ int T;
 // may the kermit bless me
 void solve()
 {
-    
+    int n; cin >> n;
+    string s; cin >> s;
+
+    string g = "";
+
+    for(int i = 0; i < n; i++){
+        if(s[i] == '4') g += "322";
+        else if (s[i] == '6') g += "53";
+        else if (s[i] == '8') g += "7222";
+        else if (s[i] == '9') g += "7332";
+        else g += s[i];
+    }
+    sort(g.rbegin(), g.rend());
+    while(g.back() == '0' || g.back() == '1') g.pop_back();
+    cout << g << nl;
 }
 
 int main()
