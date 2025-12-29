82981fb787098a6425bb7623d29caa1b39a7e74b
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-12-29 21:55:44 +0530

    inflation

--- contest2182/D.cpp
+++ contest2182/D.cpp
@@ -14,22 +14,71 @@ using vpll = vector<pair<ll, ll>>;
 #define pb push_back
 int T;
 const int MOD = 998244353;
+ll fact[60];
+void prefact()
+{
+    fact[0] = 1;
+    for (int i = 1; i <= 55; ++i)
+        fact[i] = (fact[i - 1] * i) % MOD;
+}
 // the failing attempt
 // may the kermit bless me
 
+ll calc(int n, int k)
+{
+    if (k < 0 || k > n)
+        return 0;
+    ll res = 1;
+    for (int i = 0; i < k; ++i)
+    {
+        res = (res * (n - i)) % MOD;
+    }
+    return res;
+}
+
 void solve()
 {
-    int n; cin >> n;
-    vll a(n + 1);
+    int n;
+    cin >> n;
 
-    for(auto &s : a)
+    vll a(n + 1);
+    ll sum = 0;
+
+    for(auto &s : a){
         cin >> s;
-    
+        sum += s;
+    }
+    ll k = sum / n;
+    ll left = sum % n;
+    int leftcnt = 0;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (a[i] > k + 1)
+        {
+            cout << 0 << nl;
+            return;
+        }
+        if (a[i] == k + 1)
+        {
+            leftcnt++;
+        }
+    }
+    if (leftcnt > left)
+    {
+        cout << 0 << nl;
+        return;
+    }
+    ll h = calc(left, leftcnt);
+    long long r = fact[n - leftcnt];
+    long long ans = (h * r) % MOD;
+    cout << ans << nl;
 }
 
 int main()
 {
     fastio;
+    prefact();
     int t;
     cin >> t;
 
--- contest2182/E.cpp
+++ contest2182/E.cpp
@@ -13,14 +13,39 @@ using vpii = vector<pair<int, int>>;
 using vpll = vector<pair<ll, ll>>;
 #define pb push_back
 int T;
-const int MOD = 998244353;
 // the failing attempt
 // may the kermit bless me
 
 void solve()
 {
     // n, m , k -> friends, boxes, coins
-    // friend x, y , z - > atleast a beauty of x, atleast y coins (necessary), 
+    // friend x, y , z - > atleast a beauty of x, atleast y coins (necessary), worth of happiness
+    //maximise happy friends, gift may or maynot be in a box, must purchase gifts for all with k coins
+
+    ll n, m , k;
+    cin >> n >> m >> k;
+
+    vll box(m);
+    for (auto &s : box)
+        cin >> s;
+
+    sort(box.begin(), box.end());
+    vpll fri(n);
+
+    ll sumy = 0;
+    for(int i = 0; i < n; i++){
+        ll x, y , z;
+        cin >> x >> y >> z;
+        fri[i].first = x;
+        fri[i].second = z - y;
+        sumy += y;
+    }
+    if(sumy > k){
+        cout << 0 << nl;
+        return;
+    }
+    k -= sumy;
+
 }
 
 int main()
