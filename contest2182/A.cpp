fef26fe1a81dc8cf97adaf5c97c8567efe4d4bee
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-12-29 20:04:04 +0530

    prepping contest 2182

---
+++ contest2182/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define nl "\n"
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvi = vector<vector<int>>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<ll, ll>>;
+#define pb push_back
+int T;
+// the failing attempt
+// may the kermit bless me
+
+void solve()
+{
+    
+    return;
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    // cin >> t;
+
+    for (T = 1; T <= t; T++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
