c0d1e187d1529786cc8cc498ddf8e0cacf09fe7c
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2026-01-06 07:47:53 +0530

    tle attempt

--- rating1500/1924A.cpp
+++ rating1500/1924A.cpp
@@ -11,14 +11,70 @@ using vll = vector<long long>;
 using vvi = vector<vector<int>>;
 using vpii = vector<pair<int, int>>;
 using vpll = vector<pair<ll, ll>>;
+using vc = vector<char>;
+using vs = vector<string>;
 #define pb push_back
 int T;
 // the failing attempt
 // may the kermit bless me
 
+bool exist(string subs, const string &s){
+    int n = subs.size();
+    int inisub = 0;
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == subs[inisub]){
+            inisub++;
+            if(inisub == n){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool generate(const vc &chars, string &current, int n, string &s)
+{
+    if((int)current.size() == n){
+        if(!exist(current, s)){
+            cout << "NO" << nl;
+            cout << current << nl;
+            return true;
+        }
+        return false;
+    }
+    
+    for(char c : chars){
+        current.pb(c);
+        if(generate(chars, current, n, s)) return true;
+        current.pop_back();
+    }
+    return false;
+}
+
 void solve()
 {
-    return;
+    // need to know all possible subsequences
+    // it will be k ^ n || pow(k, n)
+    // initial plan to generate all the sub sequences store them and match with the sequence m if it is not present cout which one
+
+    int n, k, m;
+    cin >> n >> k >> m;
+    string s;
+    cin >> s;
+
+    string current = "";
+
+    vc chars(k);
+    for(int i = 0; i < k; i++){
+        chars[i] = 'a' + i;
+    }
+    if(generate(chars, current, n, s)){
+        return;
+    }
+    //checking all the subs
+
+    cout << "YES" << nl;
 }
 
 int main()
