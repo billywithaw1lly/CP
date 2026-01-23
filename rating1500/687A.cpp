414d05ebcc9b79b53e90dd3cc4ecd93ea17fbb2e
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-01-24 02:50:10 +0530

    p1 from 24jan2026

--- rating1500/687A.cpp
+++ rating1500/687A.cpp
@@ -20,15 +20,93 @@ int T;
 
 const int N = 100005;
 
+vvi adj; 
+vi color;
+bool isBipartite = true;
+
+
+void bfs(int startNode)
+{
+    queue<int> q;
+    q.push(startNode);
+    color[startNode] = 1;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for (int v : adj[u])
+        {
+            if (color[v] == 0)
+            {
+                color[v] = 3 - color[u];
+                q.push(v);
+            }
+            else if (color[v] == color[u])
+            {
+                isBipartite = false;
+                return;
+            }
+        }
+    }
+}
+
 void solve()
 {
-    
+    int n, m;
+    cin >> n >> m;
+
+    adj.assign(n + 1, vector<int>());
+    color.assign(n + 1, 0);
+    isBipartite = true;
+
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (color[i] == 0)
+        {
+            bfs(i);
+            if (!isBipartite)
+            {
+                cout << -1 << "\n";
+                return;
+            }
+        }
+    }
+
+    vector<int> set1, set2;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (color[i] == 1)
+            set1.pb(i);
+        else
+            set2.pb(i);
+    }
+
+    cout << set1.size() << nl;
+    for (int i = 0; i < set1.size(); ++i)
+        cout << set1[i] << " ";
+    cout << nl;
+
+    cout << set2.size() << nl;
+    for (int i = 0; i < set2.size(); ++i)
+        cout << set2[i] << " ";
+    cout << nl;
 }
+
 int main()
 {
     fastio;
-    int t;
-    cin >> t;
+    int t = 1;
+    //cin >> t;
     for (T = 1; T <= t; T++)
     {
         solve();
