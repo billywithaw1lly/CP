01519b44df0ba51349d1ab210c9f7b4ed225b8c2
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-01-26 18:12:42 +0530

    2 problems for 26 jan

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -15,29 +15,47 @@ using vc = vector<char>;
 using vs = vector<string>;
 #define pb push_back
 
-
 int T;
 // the failing attempt
 // may the kermit bless me
 
 const int N = 100005;
-
+const int INF = 1e9;
 
 void solve()
 {
     int n; cin >> n;
     vi a(n);
 
-    for(auto &x : a){
-        cin >> x;
+    for(auto &it : a){
+        cin >> it;
     }
+
+    sort(a.rbegin(), a.rend());
+
+    vi pre(n);
+    pre[0] = a[0];
+
+    for(int i = 1; i < n; i++){
+        pre[i] = pre[i - 1] + a[i];
+    }
+
+    int left = 0;
+    int right = n - 1;
+    while(left < right){
+        int mid = (left + right) / 2;
+        if(pre[mid] < 0) right = mid - 1;
+        else if(pre[mid] >= 0) left = mid + 1;
+    }
+    cout << left << nl;
 }
 
 int main()
 {
     fastio;
     int t;
-    cin >> t;
+    //cin >> t;
+    t = 1;
     for (T = 1; T <= t; T++)
     {
         solve();
